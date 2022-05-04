183d54b106bfacacc66321c95bbc2bc59fc40b03
Rodrick <46389582+rodrigoscruz@users.noreply.github.com>
2022-05-04 17:49:50 -0300

    Begin

---
+++ server.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"net/http"
+	"fmt"
+)
+
+
+func index(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w,`
+	<h1>Olá mundo!</h1>
+	<a href="./login">Login</a>
+	`)
+}	
+
+
+func login(w http.ResponseWriter, r *http.Request){
+	http.ServeFile(w, r, "src/login.html")
+}
+
+func style(w http.ResponseWriter, r *http.Request){
+	http.ServeFile(w, r, "src/assets/style.css")
+}
+
+
+func main() {
+	http.HandleFunc("/", index)
+
+	http.HandleFunc("/login", login)
+
+	http.HandleFunc("/assets", style)
+
+	http.ListenAndServe(":80", nil)
+}
